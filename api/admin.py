cbdc08a4e5e6010601e12815f652050288ea837e
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-05-05 23:44:59 +0300

    добавлена админка

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Medication, MedicationSchedule, MedicationIntake, NotificationSettings
 
-# Register your models here.
+# Кастомизация админки для модели User
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('id', 'username', 'email', 'photoUrl')
+    search_fields = ('id', 'email', 'username')
+
+# Кастомизация админки для модели Medication
+class MedicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'form', 'unit', 'total_quantity', 'remaining_quantity', 'track_stock', 'created_at', 'updated_at')
+    search_fields = ('id', 'name', 'instructions')
+
+
+# Кастомизация админки для модели MedicationSchedule
+class MedicationScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'medication', 'frequency', 'start_date', 'end_date', 'created_at', 'updated_at')
+    search_fields = ('id', 'medication__name')
+
+# Кастомизация админки для модели MedicationIntake
+class MedicationIntakeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'schedule', 'medication', 'scheduled_date', 'scheduled_time', 'status', 'taken_at', 'created_at', 'updated_at')
+    search_fields = ('id', 'medication__name', 'schedule__id')
+
+# Кастомизация админки для модели NotificationSettings
+class NotificationSettingsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'medicationRemindersEnabled', 'minutesBeforeScheduledTime', 'lowStockRemindersEnabled')
+    search_fields = ('id', 'user__email')
+
+# Регистрация моделей в админке
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Medication, MedicationAdmin)
+admin.site.register(MedicationSchedule, MedicationScheduleAdmin)
+admin.site.register(MedicationIntake, MedicationIntakeAdmin)
+admin.site.register(NotificationSettings, NotificationSettingsAdmin)
